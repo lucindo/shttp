a51c9559f38c0cdc5808c37995ba876e2e5090a5
Renato Lucindo <lucindo@gmail.com>
2016-09-18 12:26:27 -0300

    proxy support

--- shttp.go
+++ shttp.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/facebookgo/httpdown"
@@ -41,7 +44,25 @@ func requestLogServer(handler http.Handler) http.Handler {
 }
 
 func (s *requestLogHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Proto: %v", r.Proto)
+	fmt.Println("Request:")
+	fmt.Printf("  Protocol: %v\n", r.Proto)
+	fmt.Printf("  Method: %v\n", r.Method)
+	fmt.Printf("  Content length: %v\n", r.ContentLength)
+	fmt.Printf("  URI: %v\n", r.RequestURI)
+	fmt.Printf("  Host: %v\n", r.Host)
+	fmt.Printf("  Path: %v\n", r.URL.Path)
+	fmt.Printf("  Query: %v\n", r.URL.RawQuery)
+	fmt.Println("Request headers:")
+	for key, value := range r.Header {
+		fmt.Printf("  %v: %v\n", key, formatParameterValue(value))
+	}
+	fmt.Println("Request parameters:")
+	for key, value := range r.URL.Query() {
+		fmt.Printf("  Query string parameter [%v] value [%v]\n", key, formatParameterValue(value))
+	}
+	for key, value := range r.Form {
+		fmt.Printf("  From parameter [%v] value [%v]\n", key, formatParameterValue(value))
+	}
 	s.handler.ServeHTTP(w, r)
 }
 
@@ -55,11 +76,27 @@ func logHandler(handler http.Handler, logger httpxtra.LoggerFunc, xheader bool) 
 	return &httpxtra.Handler{Handler: handler, Logger: logger, XHeaders: xheader}
 }
 
+// HTTP Proxy host
+
+func proxyHost(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Host = r.URL.Host
+		handler.ServeHTTP(w, r)
+	})
+}
+
 // Aux. functions
 
 func formatAddress(host string, port int) (string, string) {
 	address := fmt.Sprintf("%s:%d", host, port)
 	return address, fmt.Sprintf("http://%s/", address)
+}
+
+func formatParameterValue(value []string) string {
+	if len(value) == 1 {
+		return fmt.Sprintf("%v", value[0])
+	}
+	return fmt.Sprintf("%v", strings.Join(value, ","))
 }
 
 func main() {
@@ -71,18 +108,31 @@ func main() {
 	maxHeaderBytes := flag.Int("maxheaders", 1<<20, "Max header size in bytes")
 
 	// General Options
-	dir := flag.String("dir", ".", "Directory to expose")
 	open := flag.Bool("open", false, "Open a browser pointing to this server")
 	quiet := flag.Bool("quiet", false, "Do not log requests")
 	disableCors := flag.Bool("nocors", false, "Disable CORS headers")
 	logRequest := flag.Bool("debug", false, "Log request information")
 	noCache := flag.Bool("nocache", false, "Add headers disabling HTTP cache for all requests")
 
+	// Modes
+	dir := flag.String("dir", ".", "Directory to expose")
+	proxy := flag.String("proxy", "", "Act as a reverse proxy")
+
 	flag.Parse()
 
 	var handler http.Handler
 
-	handler = http.FileServer(http.Dir(*dir))
+	if *proxy != "" {
+		urlProxy, err := url.Parse(*proxy)
+		if err != nil {
+			fmt.Printf("error extracting URL from proxy parameter: %v, exiting.\n", err)
+			return
+		}
+		handler = httputil.NewSingleHostReverseProxy(urlProxy)
+		handler = proxyHost(handler)
+	} else {
+		handler = http.FileServer(http.Dir(*dir))
+	}
 
 	if !*quiet {
 		handler = logHandler(handler, logApache, true)
